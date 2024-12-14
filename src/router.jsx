f62d0a13e8f76bebb6c1d8ce5c07cc8caa59d03c
hninmohmohlwin <hninmohmohlwin16@gmail.com>
2024-12-14 19:08:15 +0630

    RegisterAndLogin

--- src/router.jsx
+++ src/router.jsx
@@ -9,41 +9,59 @@ import ProductCreate from "./pages/ProductCreate";
 import Vouncher from "./pages/Vouncher";
 import ProductEdit from "./pages/ProductEdit";
 import VoucherDetail from "./pages/VoucherDetail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element:<Layout/>,
+      
         errorElement:<NotFoundPage/>,
         children:[
             {
                 index:true,
-                element:<Dashboard/>
+                element:<Login/>
             },
             {
-                path:"/product",
-                element:<Product/>
+                path:"/register",
+                element:<Register/>
             },
             {
-                path:"/product/productCreate",
-                element:<ProductCreate/>
-            },
-            {
-                path:"/product/:id",
-                element:<ProductEdit/>
-            },
-             {
-                path:"/sale",
-                element:<Sale/>
-            },
-            {
-                path:"/vouncher",
-                element:<Vouncher/>
-            },
-            {
-                path:"/vouncher/voucherDetail/:id",
-                element:<VoucherDetail/>
+                path:"dashboard",
+                element:<Layout/>,
+                children:[
+                    {
+                        index:true,
+                        element:<Dashboard/>
+                    },
+                    {
+                       
+                        path:"product",
+                        element:<Product/>
+                    },
+                    {
+                        path:"product/productCreate",
+                        element:<ProductCreate/>
+                    },
+                    {
+                        path:"product/:id",
+                        element:<ProductEdit/>
+                    },
+                     {
+                        path:"sale",
+                        element:<Sale/>
+                    },
+                    {
+                        path:"vouncher",
+                        element:<Vouncher/>
+                    },
+                    {
+                        path:"vouncher/voucherDetail/:id",
+                        element:<VoucherDetail/>
+                    }
+                ]
             }
+           
         ]
     }
 ])
