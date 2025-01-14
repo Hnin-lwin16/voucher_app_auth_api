2cb67624d553fd3347ca283feea104d10863c990
hninmohmohlwin <hninmohmohlwin16@gmail.com>
2025-01-14 07:25:51 +0630

    Pagination

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,8 @@ import Container from '../components/Container'
 import BreadCrumb from '../components/BreadCrumb'
 import ProductList from '../components/ProductList'
 import ProductRow from '../components/ProductRow'
+import Pagination from '../components/Pagination'
+
 
 const Product = () => {
   return (
@@ -10,7 +12,7 @@ const Product = () => {
       <Container>
         <BreadCrumb currentstate={"Product Module"}/>
         <ProductList/>
-       
+      
       </Container>
     </section>
   )
